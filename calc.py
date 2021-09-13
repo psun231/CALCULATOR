f1c1e929df726d129fe613ad2750db0bc58a1c88
Pav <psunderawala@gmail.com>
2021-09-13 16:49:47 -0400

    Initial Commit

---
+++ calc.py
@@ -0,0 +1,6 @@
+print(eval(input("Enter your problem here: ")))
+while True:
+        try:
+                print(eval(input("Enter another problem: ")))
+        except:
+                print("Please try again")
